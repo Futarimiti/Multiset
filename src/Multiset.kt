528d8cdb2220fb95d2785552232e3cf9a163c36d
Futar <Futarimiti@163.com>
2022-01-23 11:47:35 +0000

    Add val `values` and `freq`

--- src/Multiset.kt
+++ src/Multiset.kt
@@ -1,4 +1,13 @@
 class Multiset<E>
 {
+    /**
+     * stores distinct values in this multiset.
+     */
+    val values : MutableList<E> = mutableListOf()
 
+    /**
+     * stores frequencies of every distinct value
+     * indices corresponding to $values.
+     */
+    val freq : MutableList<Int> = mutableListOf()
 }
